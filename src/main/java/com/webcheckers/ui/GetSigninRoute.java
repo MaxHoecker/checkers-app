2c1920a1e622440026597a57b466f0055811f54d
Jacob Kobrak <jak3703@g.rit.edu>
2019-10-06 15:00:53 -0400

    basic implementation for rendering the signin page

--- src/main/java/com/webcheckers/ui/GetSigninRoute.java
+++ src/main/java/com/webcheckers/ui/GetSigninRoute.java
@@ -1,7 +1,21 @@
 package com.webcheckers.ui;
 
-import spark.Route;
+import spark.*;
 
 public class GetSigninRoute implements Route {
 
+    //Constants
+    static final String VIEW_NAME = "/signin";
+
+    //Attributes
+    private TemplateEngine templateEngine;
+
+    public GetSigninRoute(TemplateEngine templateEngine){
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public String handle(Request request, Response response){
+        return templateEngine.render(new ModelAndView(null, VIEW_NAME));
+    }
 }
