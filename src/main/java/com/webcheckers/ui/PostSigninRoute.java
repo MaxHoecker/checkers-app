8601b04aa215aab0b307829eaa311c59e25fe95e
mjh9131 <mjh9131@rit.edu>
2019-10-06 15:01:46 -0400

    Made PostSigninRoute compileable

--- src/main/java/com/webcheckers/ui/PostSigninRoute.java
+++ src/main/java/com/webcheckers/ui/PostSigninRoute.java
@@ -1,7 +1,15 @@
 package com.webcheckers.ui;
 
+import spark.Request;
+import spark.Response;
 import spark.Route;
 
 public class PostSigninRoute implements Route {
+    public PostSigninRoute() {
+    }
 
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
 }
