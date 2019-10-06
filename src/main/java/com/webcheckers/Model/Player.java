26928f5c0c0a2f4cdbae1d1bbfb6b8d09b814586
Jacob Kobrak <jak3703@g.rit.edu>
2019-10-06 15:18:36 -0400

    expanded Player object

--- src/main/java/com/webcheckers/Model/Player.java
+++ src/main/java/com/webcheckers/Model/Player.java
@@ -3,13 +3,35 @@ package com.webcheckers.Model;
 public class Player {
 
     private String id;
+    private boolean inGame;
 
     public Player(String id){
         this.id = id;
+        this.inGame = false;
     }
 
     public String getId(){
         return this.id;
     }
 
+    public boolean inGame(){
+        return this.inGame;
+    }
+
+    public void toggleInGame(){
+        this.inGame = !this.inGame;
+    }
+
+    @Override
+    public boolean equals(Object that){
+        if(!(that instanceof Player)) return false;
+        if(that == this) return true;
+        Player other = (Player)that;
+        return other.getId().equals(this.id);
+    }
+
+    @Override
+    public int hashCode(){
+        return this.id.hashCode();
+    }
 }
