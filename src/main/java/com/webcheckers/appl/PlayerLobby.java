19e5666d4af763d76e164a16f3465db2a33ab2af
Joshua Weiss <jxw7470@g.rit.edu>
2019-10-06 15:31:56 -0400

    Update PlayerLobby.java
    added some basic functionality to player lobby

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -1,4 +1,31 @@
 package com.webcheckers.appl;
+import com.webcheckers.Model.Player;
+import java.util.ArrayList;
 
 public class PlayerLobby {
+    private int numplayers;
+    private ArrayList<Player> players;
+
+    PlayerLobby(){
+        numplayers = 0;
+    }
+    public ArrayList<Player> getPlayers(){
+        return players;
+    }
+    public int getNum(){
+        return numplayers;
+    }
+    public void addPlayer(Player x){
+        numplayers++;
+        players.add(x);
+    }
+    public void remPlayer(Player x){
+        if(numplayers > 0){
+            numplayers--;
+            players.remove(x);
+        }
+        else{
+            System.out.println("error! No players to remove");
+        }
+    }
 }
