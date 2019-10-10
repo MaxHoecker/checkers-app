bdedb1ea6d7b3513aa3525dc7987901ba2b77c63
mjh9131 <mjh9131@rit.edu>
2019-10-10 16:35:12 -0400

    Comments for the PlayerLobby class

--- src/main/java/com/webcheckers/Model/Space.java
+++ src/main/java/com/webcheckers/Model/Space.java
@@ -2,12 +2,9 @@ package com.webcheckers.Model;
 
 public class Space {
     private Piece piece;
-    //x and y coordinates
     private int cellIdx;
     private boolean isValid;
 
-
-    //no need for v since if there is a piece there it must be valid
     public Space(boolean v, int index, Piece p ){
         isValid = v;
         cellIdx = index;
