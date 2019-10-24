deb0115a1e332d07ab42ce3d8cc3856889286f8b
Joshua Weiss <jxw7470@g.rit.edu>
2019-10-24 16:05:34 -0400

    created get homeroutetest
    first version of gethomeroute test

--- src/test/java/com/webcheckers/Model/PlayerTest.java
+++ src/test/java/com/webcheckers/Model/PlayerTest.java
@@ -1,7 +1,10 @@
 package com.webcheckers.Model;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.webcheckers.Model.Player.Color;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -17,10 +20,20 @@ public class PlayerTest {
     }
 
     @Test
-    public void empty(){
+    public void Constructor_Test(){
         assertNull(CuT.getBoard());
         assertNull(CuT.getClass());
         assertNull(CuT.getColor());
+        assertNotNull(CuT.getName());
+    }
+     @Test
+    public void Functions_test(){
+        CuT.setBoard(mock(Board.class));
+        CuT.setColor(mock(Color.class));
+        CuT.setOpponent(mock(Player.class));
+        assertNotNull(CuT.getBoard());
+        assertNotNull(CuT.getClass());
+        assertNotNull(CuT.getColor());
         assertNotNull(CuT.getName());
     }
 }
