0932db413af3edd0e26025ca30b16343d787c5a0
Joshua Weiss <jxw7470@g.rit.edu>
2019-10-31 09:43:31 -0400

    updated validat move
    currently should call any open space a vaid move, will change

--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -4,12 +4,18 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.webcheckers.Model.Board;
 import com.webcheckers.Model.Move;
 import com.webcheckers.Model.Position;
+import com.webcheckers.Model.Space;
+import com.webcheckers.appl.PlayerServices;
 import com.webcheckers.util.Message;
+
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.Session;
 
 public class PostValidateMoveRoute implements Route {
 
@@ -29,18 +35,29 @@ public class PostValidateMoveRoute implements Route {
         System.err.println(param);
         Move move = gson.fromJson(param, Move.class);
         System.err.println("move: " + move.toString());
-
-        boolean x = true;
-        //x == true if valid
-        //x == false if not
-        if(x)
+        Session curSession = request.session();
+        PlayerServices name = curSession.attribute("playerServices");
+        Board game = name.gameBoard();
+        Space start = game.getAtPosition(move.getStart().getCell(), move.getStart().getRow());
+        Space end = game.getAtPosition(move.getEnd().getCell(), move.getEnd().getRow());
+        boolean validMove = true;
+        if(start.isValid() == false) {
+            return gson.toJson(Message.error("Invalid Move:Starting Square is Invalid"));
+        }
+        else if(end.isValid() == false) {
+            return gson.toJson(Message.error("Invalid Move:Ending Square is Invalid"));
+        }
+        else if(start.getOccupant() == null)
         {
-            return gson.toJson( Message.info("Valid Move"));
+            return gson.toJson(Message.error("Invalid Move:No Piece in Starting Position"));
+        }
+        else if(end.getOccupant() != null)
+        {
+            return gson.toJson(Message.error("Invalid Move:Target position already occupied"));  
         }
         else
         {
-            //ToDo include why it's invalid
-            return gson.toJson(Message.error("Invalid Move"));
+            return gson.toJson(Message.info("Valid Move"));
         }
     }
 }
