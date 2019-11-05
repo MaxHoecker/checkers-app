91108ab963eb2daefce940fff6e1d4dc753b3518
Jacob Kobrak <jak3703@g.rit.edu>
2019-11-05 17:18:50 -0500

    "unit tests"

--- src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.gson.Gson;
 import com.webcheckers.Model.Player;
 import com.webcheckers.appl.PlayerLobby;
 
@@ -17,108 +18,108 @@ import spark.Response;
 import spark.Session;
 import spark.TemplateEngine;
 
-
+/*
 @Tag("UI-tier")
 public class GetHomeRouteTest {
-//    //Component under test
-//    private GetHomeRoute CuT;
-//
-//    //mock objects
-//    private Request request;
-//    private Response response;
-//    private TemplateEngine templateEngine;
-//    private PlayerLobby playerLobby;
-//    private Session session;
-//
-//    //HTML constant
-//    private String TITLE_ATTR = GetHomeRoute.TITLE_ATTR;
-//    private String TITLE_TAGS = "<title>Web Checkers | " + TITLE_ATTR + "</title>";
-//    private String PLAYER_LIST_ATTR = GetHomeRoute.PLAYER_LIST_ATTR;
-//    private String CUR_PLAYER_ATTR = GetHomeRoute.CUR_PLAYER_ATTR;
-//    private String NUM_PLAYERS_ATTR = GetHomeRoute.NUM_PLAYERS_ATTR;
-//    private String IS_SIGNED_IN = GetHomeRoute.IS_SIGNED_IN;
-//
-//
-//    @BeforeEach
-//    public void setup(){
-//        request = mock(Request.class);
-//        session = mock(Session.class);
-//        when(request.session()).thenReturn(session);
-//        response = mock(Response.class);
-//        templateEngine = mock(TemplateEngine.class);
-//        playerLobby = mock(PlayerLobby.class);
-//
-//        CuT = new GetHomeRoute(playerLobby, templateEngine);
-//    }
-//
-//    @Test
-//    public void render_page(){
-//        final TemplateEngineTester testHelper = new TemplateEngineTester();
-//        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-//
-//        CuT.handle(request, response);
-//
-//        testHelper.assertViewModelExists();
-//        testHelper.assertViewModelIsaMap();
-//        testHelper.assertViewModelAttribute("title", "Welcome!");
-//        testHelper.assertViewName(GetHomeRoute.VIEW_NAME);
-//
-//    }
-//
-//    @Test
-//    public void test_NO_SIGNIN_FIRST_USER(){
-//        final TemplateEngineTester testHelper = new TemplateEngineTester();
-//        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-//        request.session().attribute(SIGNEDIN, false);
-//
-//        CuT.handle(request, response);
-//
-//        testHelper.assertViewModelAttribute("title", "Welcome!");
-//        testHelper.assertViewModelAttribute("numPlayers", 0);
-//        testHelper.assertViewModelAttribute("isSignedIn", false);
-//    }
-//
-//    @Test
-//    public void test_SIGNIN_FIRST_USER(){
-//        final TemplateEngineTester testHelper = new TemplateEngineTester();
-//        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-//        request.session().attribute(SIGNEDIN, true);
-//
-//        CuT.handle(request, response);
-//
-//        testHelper.assertViewModelAttribute("title", "Welcome!");
-//        testHelper.assertViewModelAttribute("numPlayers", 0);
-//        //testHelper.assertViewModelAttribute("isSignedIn", true);
-//    }
-//
-//    @Test
-//    public void test_NOT_SIGNED_IN_SECOND_USER(){
-//        final TemplateEngineTester testHelper = new TemplateEngineTester();
-//        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-//        request.session().attribute(SIGNEDIN, false);
-//        playerLobby.addPlayer("Josh", new Player("Josh"));
-//        request.session().attribute(NUM_PLAYERS_ATTR, playerLobby.getNumPlayers());
-//
-//        CuT.handle(request, response);
-//
-//        testHelper.assertViewModelAttribute("title", "Welcome!");
-//        //testHelper.assertViewModelAttribute("numPlayers", 1);
-//        testHelper.assertViewModelAttribute("isSignedIn", false);
-//    }
-//
-//    @Test
-//    public void test_SIGNED_IN_SECOND_USER(){
-//        final TemplateEngineTester testHelper = new TemplateEngineTester();
-//        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-//        request.session().attribute(SIGNEDIN, true);
-//        playerLobby.addPlayer("Josh", new Player("Josh"));
-//        request.session().attribute(NUM_PLAYERS_ATTR, playerLobby.getNumPlayers());
-//
-//        CuT.handle(request, response);
-//
-//        testHelper.assertViewModelAttribute("title", "Welcome!");
-        //testHelper.assertViewModelAttribute("numPlayers", 1);
-        //testHelper.assertViewModelAttribute("isSignedIn", true);
-        //testHelper.assertViewModelAttribute("playerList", "Josh");
+    //Component under test
+    private GetHomeRoute CuT;
+
+    //mock objects
+    private Request request;
+    private Response response;
+    private TemplateEngine templateEngine;
+    private PlayerLobby playerLobby;
+    private Session session;
+
+    //HTML constant
+    private String TITLE_ATTR = GetHomeRoute.TITLE_ATTR;
+    private String TITLE_TAGS = "<title>Web Checkers | " + TITLE_ATTR + "</title>";
+    private String PLAYER_LIST_ATTR = GetHomeRoute.PLAYER_LIST_ATTR;
+    private String CUR_PLAYER_ATTR = GetHomeRoute.CUR_PLAYER_ATTR;
+    private String NUM_PLAYERS_ATTR = GetHomeRoute.NUM_PLAYERS_ATTR;
+    private String IS_SIGNED_IN = GetHomeRoute.IS_SIGNED_IN;
+
+    //Friendly
+    Gson gson = new Gson();
+
+
+    @BeforeEach
+    public void setup(){
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        response = mock(Response.class);
+        templateEngine = mock(TemplateEngine.class);
+        playerLobby = mock(PlayerLobby.class);
+
+        CuT = new GetHomeRoute(playerLobby, templateEngine, gson);
     }
-//}
+
+    @Test
+    public void render_page(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        CuT.handle(request, response);
+
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        testHelper.assertViewModelAttribute("title", "Welcome!");
+        testHelper.assertViewName(GetHomeRoute.VIEW_NAME);
+
+    }
+
+    @Test
+    public void test_NO_SIGNIN_FIRST_USER(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+        request.session().attribute(SIGNEDIN, false);
+
+        CuT.handle(request, response);
+
+        testHelper.assertViewModelAttribute("title", "Welcome!");
+        testHelper.assertViewModelAttribute("numPlayers", 0);
+        testHelper.assertViewModelAttribute("isSignedIn", false);
+    }
+
+    @Test
+    public void test_SIGNIN_FIRST_USER(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+        request.session().attribute(SIGNEDIN, true);
+
+        CuT.handle(request, response);
+
+        testHelper.assertViewModelAttribute("title", "Welcome!");
+        testHelper.assertViewModelAttribute("numPlayers", 0);
+        //testHelper.assertViewModelAttribute("isSignedIn", true);
+    }
+
+    @Test
+    public void test_NOT_SIGNED_IN_SECOND_USER(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+        request.session().attribute(SIGNEDIN, false);
+        playerLobby.addPlayer("Josh", new Player("Josh"));
+        request.session().attribute(NUM_PLAYERS_ATTR, playerLobby.getNumPlayers());
+
+        CuT.handle(request, response);
+
+        testHelper.assertViewModelAttribute("title", "Welcome!");
+        //testHelper.assertViewModelAttribute("numPlayers", 1);
+        testHelper.assertViewModelAttribute("isSignedIn", false);
+    }
+
+    @Test
+    public void test_SIGNED_IN_SECOND_USER(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+        request.session().attribute(SIGNEDIN, true);
+        playerLobby.addPlayer("Josh", new Player("Josh"));
+        request.session().attribute(NUM_PLAYERS_ATTR, playerLobby.getNumPlayers());
+
+        CuT.handle(request, response);
+
+        testHelper.assertViewModelAttribute("title", "Welcome!");//testHelper.assertViewModelAttribute("numPlayers", 1)//testHelper.assertViewModelAttribute("isSignedIn", true)//testHelper.assertViewModelAttribute("playerList", "Josh"); }
+}
+*/
--- src/test/java/com/webcheckers/ui/PostSigninRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostSigninRouteTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.when;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import com.webcheckers.Model.Player;
 import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.appl.PlayerServices;
 import javafx.geometry.Pos;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -22,94 +23,79 @@ import spark.Request;
 import spark.Response;
 import spark.Session;
 import spark.TemplateEngine;
+import spark.template.freemarker.FreeMarkerEngine;
 
 @Tag("UI-Tier")
 public class PostSigninRouteTest {
 
-//    private static final String TEST = "Test123";
-//    private static final String TEST_NO = "   ";
-//    private static final String INVALID_U = "!!!!222";
-//    private static final String SAME_U = "Same username";
-//
-//    private Request request;
-//    private Session session;
-//    private Response response;
-//    private TemplateEngine engine;
-//    private PlayerLobby playerLobby;
-//    private Player player;
-//    private Player playerWrong;
-//
-//    private PostSigninRoute CuT;
-//
-//    @BeforeEach
-//    public void setup(){
-//        request = mock(Request.class);
-//        session = mock(Session.class);
-//        when(request.session()).thenReturn(session);
-//        engine = mock(TemplateEngine.class);
-//        response = mock(Response.class);
-//
-//        playerLobby = mock(PlayerLobby.class);
-//        player = new Player(TEST);
-//        playerWrong = new Player(TEST_NO);
-//
-//        CuT = new PostSigninRoute(playerLobby,engine);
-//
-//    }
-//
-//    @Test
-//    public void bad_username(){
-//        when(request.queryParams(eq(PostSigninRoute.USERNAME_PARAM))).thenReturn(INVALID_U);
-//
-//        final TemplateEngineTester testHelper = new TemplateEngineTester();
-//        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-//
-//        CuT.handle(request, response);
-//
-//        testHelper.assertViewModelIsaMap();
-//        testHelper.assertViewModelExists();
-//
-//        //testHelper.assertViewName(PostSigninRoute.INVALID_NAME_MSG)
-//
-//
-//        testHelper.assertViewModelAttributeIsAbsent(PostSigninRoute.NAME_TAKEN_MSG);
-//        testHelper.assertViewModelAttributeIsAbsent(PostSigninRoute.USERNAME_PARAM);
-//
-//
-//
-//    }
-//
-//    @Test
-//    public void same_username(){
-//        when(request.queryParams(eq(PostSigninRoute.USERNAME_PARAM))).thenReturn(SAME_U);
-//
-//        final TemplateEngineTester testHelper = new TemplateEngineTester();
-//        when(engine.render((any(ModelAndView.class)))).thenAnswer(testHelper.makeAnswer());
-//
-//        CuT.handle(request,response);
-//
-//        testHelper.assertViewModelIsaMap();
-//        testHelper.assertViewModelExists();
-//
-//        //testHelper.assertViewName(PostSigninRoute.NAME_TAKEN_MSG);
-//
-//    }
-//
-//    @Test
-//    public void correct_username(){
-//        when(request.queryParams(eq(PostSigninRoute.USERNAME_PARAM))).thenReturn(TEST);
-//
-//
-//        final TemplateEngineTester testHelper = new TemplateEngineTester();
-//        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-//
-//        CuT.handle(request, response);
-//
-//        testHelper.assertViewModelExists();
-//        testHelper.assertViewModelIsaMap();
-//
-//
-//    }
+    private static final String TEST = "Test123";
+    private static final String TEST_NO = "   ";
+    private static final String INVALID_U = "\"Josh\"";
+    private static final String SAME_U = "Same username";
+    private static final String VALID = "Jake";
+
+    static final String NAME_TAKEN_MSG = "Username taken. Please enter another name.";
+    static final String INVALID_NAME_MSG = "Invalid username. Please enter another name.";
+
+    private Request request;
+    private Session session;
+    private Response response;
+    private TemplateEngine engine;
+    private PlayerLobby playerLobby;
+    private Player player;
+    private Player playerWrong;
+    private PlayerServices playerServices;
+
+    private PostSigninRoute CuT;
+
+    @BeforeEach
+    public void setup(){
+        request = mock(Request.class);
+        session = mock(Session.class);
+        playerServices = mock(PlayerServices.class);
+        engine = mock(FreeMarkerEngine.class);
+        when(request.session()).thenReturn(session);
+        response = mock(Response.class);
+
+        playerLobby = mock(PlayerLobby.class);
+        player = new Player(TEST);
+        playerWrong = new Player(TEST_NO);
+
+        CuT = new PostSigninRoute(playerLobby,engine);
+    }
+
+    @Test
+    public void first_successful(){
+        when(request.queryParams(any(String.class))).thenReturn(VALID);
+        when(playerServices.setCurPlayer(VALID)).thenReturn(null);
+
+        String result = CuT.handle(request, response);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void invalid_username(){
+        when(request.queryParams(any(String.class))).thenReturn(INVALID_U);
+        when(playerServices.setCurPlayer(INVALID_U)).thenReturn(INVALID_NAME_MSG);
+        when(engine.render(any(ModelAndView.class))).thenReturn("any(String.class)");
+
+        String result = CuT.handle(request, response);
+
+        assertNotNull(result);
+    }
+
+    @Test
+    public void username_taken(){
+        when(request.queryParams(any(String.class))).thenReturn(SAME_U);
+        when(playerServices.setCurPlayer(SAME_U)).thenReturn(NAME_TAKEN_MSG);
+        when(engine.render(any(ModelAndView.class))).thenReturn("any(String.class)");
+
+        String result = CuT.handle(request, response);
+
+        assertNotNull(result);
+
+    }
 
 
 
