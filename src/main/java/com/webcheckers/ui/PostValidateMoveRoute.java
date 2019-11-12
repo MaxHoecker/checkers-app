afd37ceb6f81972a18cc2c28a9ca6c4674577716
Jacob Kobrak <jak3703@g.rit.edu>
2019-11-12 16:04:50 -0500

    fixed master merge issue; PostValidateMove.java never got updated here for some reason

--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -1,15 +1,14 @@
 package com.webcheckers.ui;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.webcheckers.Model.Move;
-import com.webcheckers.Model.Position;
+import com.webcheckers.appl.PlayerServices;
 import com.webcheckers.util.Message;
+
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.Session;
 
 public class PostValidateMoveRoute implements Route {
 
@@ -22,26 +21,14 @@ public class PostValidateMoveRoute implements Route {
     public PostValidateMoveRoute(final Gson gson){
         this.gson = gson;
     }
-
+    @Override
     public String handle(Request request, Response response){
 
         String param = request.queryParams(ACTION_DATA_PARAM);
-        System.err.println(param);
-        Move move = gson.fromJson(param, Move.class);
-        System.err.println("move: " + move.toString());
+        Session curSession = request.session();
+        PlayerServices playerServices = curSession.attribute("playerServices");
 
-
-        boolean x = true;
-        //x == true if valid
-        //x == false if not
-        if(x)
-        {
-            return gson.toJson( new Message("Valid Move", Message.Type.INFO));
-        }
-        else
-        {
-            //ToDo include why it's invalid
-            return gson.toJson(new Message("Invalid Move", Message.Type.ERROR));
-        }
+        Message msg = playerServices.validateMove(param);
+        return gson.toJson(msg);
     }
 }
