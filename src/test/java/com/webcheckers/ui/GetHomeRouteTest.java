a324664f94d2159463d689ff0c3b200e88b1cb3f
jdr1857@g.rit.edu <Jdr0923!>
2019-11-17 15:29:40 -0500

    New branch created for unit tests

--- src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
@@ -18,7 +18,7 @@ import spark.Response;
 import spark.Session;
 import spark.TemplateEngine;
 
-/*
+
 @Tag("UI-tier")
 public class GetHomeRouteTest {
     //Component under test
@@ -71,55 +71,20 @@ public class GetHomeRouteTest {
 
     @Test
     public void test_NO_SIGNIN_FIRST_USER(){
-        final TemplateEngineTester testHelper = new TemplateEngineTester();
-        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-        request.session().attribute(SIGNEDIN, false);
 
-        CuT.handle(request, response);
-
-        testHelper.assertViewModelAttribute("title", "Welcome!");
-        testHelper.assertViewModelAttribute("numPlayers", 0);
-        testHelper.assertViewModelAttribute("isSignedIn", false);
     }
 
     @Test
     public void test_SIGNIN_FIRST_USER(){
-        final TemplateEngineTester testHelper = new TemplateEngineTester();
-        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-        request.session().attribute(SIGNEDIN, true);
 
-        CuT.handle(request, response);
-
-        testHelper.assertViewModelAttribute("title", "Welcome!");
-        testHelper.assertViewModelAttribute("numPlayers", 0);
-        //testHelper.assertViewModelAttribute("isSignedIn", true);
     }
 
     @Test
     public void test_NOT_SIGNED_IN_SECOND_USER(){
-        final TemplateEngineTester testHelper = new TemplateEngineTester();
-        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-        request.session().attribute(SIGNEDIN, false);
-        playerLobby.addPlayer("Josh", new Player("Josh"));
-        request.session().attribute(NUM_PLAYERS_ATTR, playerLobby.getNumPlayers());
 
-        CuT.handle(request, response);
-
-        testHelper.assertViewModelAttribute("title", "Welcome!");
-        //testHelper.assertViewModelAttribute("numPlayers", 1);
-        testHelper.assertViewModelAttribute("isSignedIn", false);
     }
 
     @Test
-    public void test_SIGNED_IN_SECOND_USER(){
-        final TemplateEngineTester testHelper = new TemplateEngineTester();
-        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-        request.session().attribute(SIGNEDIN, true);
-        playerLobby.addPlayer("Josh", new Player("Josh"));
-        request.session().attribute(NUM_PLAYERS_ATTR, playerLobby.getNumPlayers());
-
-        CuT.handle(request, response);
-
-        testHelper.assertViewModelAttribute("title", "Welcome!");//testHelper.assertViewModelAttribute("numPlayers", 1)//testHelper.assertViewModelAttribute("isSignedIn", true)//testHelper.assertViewModelAttribute("playerList", "Josh"); }
+    public void test_SIGNED_IN_SECOND_USER() {
+    }
 }
-*/
