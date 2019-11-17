b29daa1e8f43f49e18236424963aba33f841a2e5
jdr1857@g.rit.edu <Jdr0923!>
2019-11-17 17:03:17 -0500

    GameTest skeleton created

--- src/test/java/com/webcheckers/Model/GameTest.java
+++ src/test/java/com/webcheckers/Model/GameTest.java
@@ -1,4 +1,42 @@
 package com.webcheckers.Model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("Model-Tier")
 public class GameTest {
+
+    @Test
+    public void test_init_move(){
+
+    }
+
+    @Test
+    public void test_change_board_state(){
+
+    }
+
+    @Test
+    public void test_kinging_red(){
+
+    }
+
+    @Test
+    public void test_kinging_white(){
+
+    }
+
+    @Test
+    public void test_swap_turn_red(){
+
+    }
+
+    @Test
+    public void test_swap_turn_white(){
+
+    }
+
 }
